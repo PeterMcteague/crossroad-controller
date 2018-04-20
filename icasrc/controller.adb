82d79508c922b89c17359343b3e6dd989190319f
Peter Mcteague <07mcteaguepet@googlemail.com>
2018-04-20 19:28:28 +0100

    Updated to use proper timings

--- icasrc/controller.adb
+++ icasrc/controller.adb
@@ -1,33 +1,70 @@
 with HWIF; use HWIF;
 with HWIF_Types; use HWIF_Types;
+With Ada.Calendar; use Ada.Calendar;
 
 procedure Controller is
    Delays : constant array (1..11) of Duration := (2.0,2.0,5.0,3.0,2.0,2.0,5.0,3.0,2.0,6.0,6.0);
    State : Integer := 1;
-   NextState: Integer;
+   NextState: Integer := 1;
+   Time_Next : Ada.Calendar.Time;
 
+   --Sets the button to change after 0.2 seconds--
    procedure CheckPedestrianButton is
+      Inner_NextTime: Ada.Calendar.Time;
    begin
-      if (Pedestrian_Button(North) = 1 or Pedestrian_Button(South) = 1) then
-         Pedestrian_Wait(North) := 1;
-         Pedestrian_Wait(South) := 1;
-      end if;
-
-      if (Pedestrian_Button(East) = 1 or Pedestrian_Button(West) = 1) then
-         Pedestrian_Wait(East) := 1;
-         Pedestrian_Wait(West) := 1;
+      if (Pedestrian_Button(North) = 1 or Pedestrian_Button(South) = 1)
+        or (Pedestrian_Button(East) = 1 or Pedestrian_Button(West) = 1) then
+         Inner_NextTime := Clock + 0.2;
+         if (Pedestrian_Button(North) = 1 or Pedestrian_Button(South) = 1) then
+            while Pedestrian_Wait(North) = 0 and Pedestrian_Wait(South) = 0 loop
+               if Ada.Calendar."<="(Inner_NextTime, Ada.Calendar.Clock) then
+                  Pedestrian_Wait(North) := 1;
+                  Pedestrian_Wait(South) := 1;
+               end if;
+            end loop;
+         end if;
+         if (Pedestrian_Button(East) = 1 or Pedestrian_Button(West) = 1) then
+            while Pedestrian_Wait(East) = 0 and Pedestrian_Wait(West) = 0 loop
+               if Ada.Calendar."<="(Inner_NextTime, Ada.Calendar.Clock) then
+                  Pedestrian_Wait(East) := 1;
+                  Pedestrian_Wait(West) := 1;
+               end if;
+            end loop;
+         end if;
       end if;
    end CheckPedestrianButton;
+
+   procedure CheckEV is
+   begin
+       if State = 3 then
+            if Emergency_Vehicle_Sensor(North) = 1 or Emergency_Vehicle_Sensor(South) = 1 then
+               while Emergency_Vehicle_Sensor(North) = 1 or Emergency_Vehicle_Sensor(South) = 1 loop
+                  null;
+               end loop;
+               Time_Next := Clock + 5.0; --Normal delay plus EV delay is 10--
+            end if;
+      end if;
+
+      if State = 7 then
+            if Emergency_Vehicle_Sensor(East) = 1 or Emergency_Vehicle_Sensor(West) = 1 then
+               while Emergency_Vehicle_Sensor(East) = 1 or Emergency_Vehicle_Sensor(West) = 1 loop
+                  null;
+               end loop;
+               Time_Next := Clock + 5.0; --Normal delay plus EV delay is 10--
+            end if;
+      end if;
+   end CheckEV;
 begin
    Endless_Loop :
    loop
+      --Need to do something about these delays here--
       CheckPedestrianButton;
+      CheckEV;
 
-      -- Checking for EV
-      -- Checking for pdesestrian
-
-      -- if Ada.Calendar."<="(Time_Next, Ada.Calendar.Clock) then
-
+      --Check whether to swap to nextstate
+      if NextState /= State and Ada.Calendar."<="(Time_Next, Ada.Calendar.Clock) then
+         State := NextState;
+      end if;
 
       case State is
          when 1 => --All red--
@@ -40,8 +77,6 @@ begin
             Pedestrian_Light(South) := 2;
             Pedestrian_Light(West) := 2;
 
-            -- Set a Next_State := Clock + Delays(State)
-
          when 2 => --NS RA--
             Traffic_Light(North) := 6;
             Traffic_Light(South) := 6;
@@ -49,14 +84,6 @@ begin
          when 3 => --NS G--
             Traffic_Light(North) := 1;
             Traffic_Light(South) := 1;
-
-            --If emergency-
-            if Emergency_Vehicle_Sensor(North) = 1 or Emergency_Vehicle_Sensor(South) = 1 then
-               while Emergency_Vehicle_Sensor(North) = 1 or Emergency_Vehicle_Sensor(South) = 1 loop
-                  CheckPedestrianButton; --I'm not looking to do anything, but checking for pedestrian buttons should be done because of how long we could potentially be here
-               end loop;
-               delay 5.0; --Delayed for 5s as 5+5=10 , for req
-            end if;
 
          when 4 => --NS A--
             Traffic_Light(North) := 2;
@@ -73,14 +100,6 @@ begin
          when 7 => --EW G--
             Traffic_Light(East) := 1;
             Traffic_Light(West) := 1;
-
-            --If emergency-
-            if Emergency_Vehicle_Sensor(East) = 1 or Emergency_Vehicle_Sensor(West) = 1 then
-               while Emergency_Vehicle_Sensor(East) = 1 or Emergency_Vehicle_Sensor(West) = 1 loop
-                  CheckPedestrianButton; --I'm not looking to do anything, but checking for pedestrian buttons should be done because of how long we could potentially be here
-               end loop;
-               delay 5.0; --Delayed for 5s as 5+5=10 , for req
-            end if;
 
          when 8 => --EW A--
             Traffic_Light(East) := 2;
@@ -108,11 +127,15 @@ begin
             null;
       end case;
 
-      CheckPedestrianButton; --Lets check it again
+      --Setting up the delay first time--
+      if NextState = State then
+         Time_Next := Clock + Delays(State);
+      end if;
 
       --Checking what the next state should be--
       case State is
 
+         --If all red--
          when 1 =>
             --When N or S pedestrian buttons pressed and at an all red--
             if (Pedestrian_Wait(North) = 1 or Pedestrian_Wait(South) = 1)
@@ -126,21 +149,17 @@ begin
                NextState := State + 1;
             end if;
 
-
+         --If at the end of the natural cycle go back to the start--
          when 9 =>
             NextState := 1;
 
-         --Go back to all red from pedestrian green
+         --Go back to all red from pedestrian green--
          when 10..11 =>
             NextState := 1;
 
          when others =>
             NextState := State + 1;
       end case;
-
-      --Delay for length of state and then swap to the next state--
-      delay Delays(State);
-      State := NextState;
 
    end loop Endless_Loop;
 end Controller;
