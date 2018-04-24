dd1b66e9159fdb49515a1c2cd6af21ca6273568f
Peter Mcteague <07mcteaguepet@googlemail.com>
2018-04-24 19:51:50 +0100

    Added comments, shifted pedestrian lights to the end of each light cycle rather than all.
    This commit had no scores running on my windows PC for 1800 seconds. The current commit only needs the linux tests.

--- icasrc/controller.adb
+++ icasrc/controller.adb
@@ -3,29 +3,31 @@ with HWIF_Types; use HWIF_Types;
 With Ada.Calendar; use Ada.Calendar;
 
 procedure Controller is
-   Delays : constant array (1..11) of Duration := (2.0,2.0,5.0,3.0,2.0,2.0,5.0,3.0,2.0,6.0,6.0);
-   State : Integer := 1;
-   NextState: Integer := 2;
-   PreviousState: Integer := 1;
-   Time_Next : Ada.Calendar.Time;
-   EV_Incoming_NS : Boolean := False;
+   Delays : constant array (1..13) of Duration := (0.5,6.0,0.5,0.5,5.0,3.0,0.5,6.0,0.5,0.5,5.0,3.0,0.5); --Manages lengths of states--
+   State : Integer := 1; --The start state--
+   NextState: Integer; --The state to swap to after state--
+   Time_Next : Ada.Calendar.Time; --Stores a calendar time, used for delays--
+   EV_Incoming_NS : Boolean := False; --Whether an emergency vehicle is incoming--
    EV_Incoming_EW : Boolean := False;
 
-   --Sets the button to change after 0.2 seconds--
+   --Checks whether a pedestrian button has been pressed--
    procedure CheckPedestrianButton is
    begin
-      if (Pedestrian_Button(North) = 1 or Pedestrian_Button(South) = 1) and State /= 10
+      --If the north or south buttons been pressed and the green pedestrian light isn't on and the wait light isn't already on, turn the wait light on--
+      if (Pedestrian_Button(North) = 1 or Pedestrian_Button(South) = 1) and State /= 2
          and (Pedestrian_Wait(North) /= 1 and Pedestrian_Wait(South) /= 1) then
          Pedestrian_Wait(North) := 1;
          Pedestrian_Wait(South) := 1;
       end if;
-      if (Pedestrian_Button(East) = 1 or Pedestrian_Button(West) = 1) and State /= 11
+      --If the east or west buttons been pressed and the green pedestrian light isn't on and the wait light isn't already on, turn the wait light on--
+      if (Pedestrian_Button(East) = 1 or Pedestrian_Button(West) = 1) and State /= 8
       and (Pedestrian_Wait(East) /= 1 and Pedestrian_Wait(West) /= 1)then
          Pedestrian_Wait(East) := 1;
          Pedestrian_Wait(West) := 1;
       end if;
    end CheckPedestrianButton;
 
+   --If the emergency vehicle sensors pick something up, mark an EV as incoming--
    procedure CheckEV is
    begin
       if (Emergency_Vehicle_Sensor(North) = 1 or Emergency_Vehicle_Sensor(South) = 1) then
@@ -37,10 +39,13 @@ procedure Controller is
       end if;
    end CheckEV;
 
+   --If there's a emergency vehicle hold this state--
    procedure HoldEV is
       Inner_NextTime: Ada.Calendar.Time;
    begin
-      if State = 3 and EV_Incoming_NS then
+      --If we're on green for NS and there's an EV incoming
+      if State = 5 and EV_Incoming_NS then
+         --Stay until it's gone--
          while Emergency_Vehicle_Sensor(North) = 1 or Emergency_Vehicle_Sensor(South) = 1 loop
             CheckPedestrianButton;
             CheckEV;
@@ -51,10 +56,13 @@ procedure Controller is
             CheckPedestrianButton;
             CheckEV;
          end loop;
+         --Mark that there's no longer an EV incoming--
          EV_Incoming_NS := False;
       end if;
 
-      if State = 7 and EV_Incoming_EW then
+      --If we're on green for EW and there's an EV incoming
+      if State = 11 and EV_Incoming_EW then
+         --Stay until it's gone--
          while Emergency_Vehicle_Sensor(East) = 1 or Emergency_Vehicle_Sensor(West) = 1 loop
             CheckPedestrianButton;
             CheckEV;
@@ -65,15 +73,20 @@ procedure Controller is
             CheckPedestrianButton;
             CheckEV;
          end loop;
+         --Mark that there's no longer an EV incoming--
          EV_Incoming_EW := False;
       end if;
    end HoldEV;
+
 begin
    Endless_Loop :
    loop
+
+      --Check for buttons and emergency vehicles right off--
       CheckPedestrianButton;
       CheckEV;
 
+      --Change lights depending on state / set state--
       case State is
          when 1 => --All red--
             Traffic_Light(North) := 4;
@@ -85,65 +98,71 @@ begin
             Pedestrian_Light(South) := 2;
             Pedestrian_Light(West) := 2;
 
-         when 2 => --NS RA--
-            Traffic_Light(North) := 6;
-            Traffic_Light(South) := 6;
-
-         when 3 => --NS G--
-            Traffic_Light(North) := 1;
-            Traffic_Light(South) := 1;
-
-         when 4 => --NS A--
-            Traffic_Light(North) := 2;
-            Traffic_Light(South) := 2;
-
-         when 5 => --NS R--
-            Traffic_Light(North) := 4;
-            Traffic_Light(South) := 4;
-
-         when 6 => --EW RA--
-            Traffic_Light(East) := 6;
-            Traffic_Light(West) := 6;
-
-         when 7 => --EW G--
-            Traffic_Light(East) := 1;
-            Traffic_Light(West) := 1;
-
-         when 8 => --EW A--
-            Traffic_Light(East) := 2;
-            Traffic_Light(West) := 2;
-
-         when 9 => --EW R--
-            Traffic_Light(East) := 4;
-            Traffic_Light(West) := 4;
-
-         --The below states are out of cycle. These are reached conditionally.
-
-         when 10 => --NS Pedestrian lights G--
+         when 2 => --NS Pedestrians G--
             Pedestrian_Light(North) := 1;
             Pedestrian_Light(South) := 1;
             Pedestrian_Wait(North) := 0;
             Pedestrian_Wait(South) := 0;
 
-         when 11 => --EW Pedestrian lights G--
+         when 3 => --NS Pedestrians R--
+            Pedestrian_Light(North) := 2;
+            Pedestrian_Light(South) := 2;
+
+         when 4 => --NS RA--
+            Traffic_Light(North) := 6;
+            Traffic_Light(South) := 6;
+
+         when 5 => --NS G--
+            Traffic_Light(North) := 1;
+            Traffic_Light(South) := 1;
+
+         when 6 => --NS A--
+            Traffic_Light(North) := 2;
+            Traffic_Light(South) := 2;
+
+         when 7 => --NS R--
+            Traffic_Light(North) := 4;
+            Traffic_Light(South) := 4;
+
+         when 8 => --EW Pedestrians G--
             Pedestrian_Light(East) := 1;
             Pedestrian_Light(West) := 1;
             Pedestrian_Wait(East) := 0;
             Pedestrian_Wait(West) := 0;
 
+         when 9 => --EW Pedestrians R--
+            Pedestrian_Light(East) := 2;
+            Pedestrian_Light(West) := 2;
+
+         when 10 => --EW RA--
+            Traffic_Light(East) := 6;
+            Traffic_Light(West) := 6;
+
+         when 11 => --EW G--
+            Traffic_Light(East) := 1;
+            Traffic_Light(West) := 1;
+
+         when 12 => --EW A--
+            Traffic_Light(East) := 2;
+            Traffic_Light(West) := 2;
+
+         when 13 => --EW R--
+            Traffic_Light(East) := 4;
+            Traffic_Light(West) := 4;
+
          when others =>
             null;
       end case;
 
-      --loop until next state with checks for EV and Pedestriain buttons--
+      --Wait for set state length whilst checks for EV and Pedestriain buttons--
       Time_Next := Clock + Delays(State);
       while Ada.Calendar.">="(Time_Next, Ada.Calendar.Clock) loop
          CheckPedestrianButton;
          CheckEV;
       end loop;
 
-      --Checking for special state actions--
-      if (State = 3 and EV_Incoming_NS) or (State = 7 and EV_Incoming_EW) then
+      --Check if we should hold the state for an emergency vehicle--
+      if (State = 5 and EV_Incoming_NS) or (State = 11 and EV_Incoming_EW) then
          HoldEV;
       end if;
 
@@ -152,34 +171,47 @@ begin
 
          --If all red--
          when 1 =>
-            --When N or S pedestrian buttons pressed and at an all red and there's no EV--
-            if (Pedestrian_Wait(North) = 1 or Pedestrian_Wait(South) = 1)
-              and (Emergency_Vehicle_Sensor(North) /= 1 and Emergency_Vehicle_Sensor(South) /= 1)
-              and (Emergency_Vehicle_Sensor(East) /= 1 and Emergency_Vehicle_Sensor(West) /= 1)
-              and (PreviousState /= 10) then
+            if EV_Incoming_EW then
                NextState := 10;
-            --When E or W pedestrian buttons pressed and at an all red and there's no EV--
-            elsif (Pedestrian_Wait(East) = 1 or Pedestrian_Wait(West) = 1)
-              and (Emergency_Vehicle_Sensor(East) /= 1 and Emergency_Vehicle_Sensor(West) /= 1)
-              and (Emergency_Vehicle_Sensor(North) /= 1 and Emergency_Vehicle_Sensor(South) /= 1)
-              and (PreviousState /= 11) then
-               NextState := 11;
-            --If we're at all red and there's an EW EV , put EW to RA instead of NS--
-            elsif (Emergency_Vehicle_Sensor(East) = 1 or Emergency_Vehicle_Sensor(West) = 1) then
-               NextState := 6;
+            elsif EV_Incoming_NS then
+               NextState := 4;
+            elsif (Pedestrian_Wait(North) = 1 and Pedestrian_Wait(South) = 1)
+              and EV_Incoming_NS = False and EV_Incoming_EW = False then
+               NextState := 2;
             else
-               NextState := State + 1;
+               NextState := 4;
             end if;
 
-         --Go back to all red from pedestrian green or end of cycle--
-         when 9..11 =>
-            NextState := 1;
+         --If NS red--
+         when 7 =>
+            if EV_Incoming_NS then
+               NextState := 4;
+            elsif EV_Incoming_EW then
+               NextState := 10;
+            elsif (Pedestrian_Wait(East) = 1 and Pedestrian_Wait(West) = 1)
+              and EV_Incoming_NS = False and EV_Incoming_EW = False then
+               NextState := 8;
+            else
+               NextState := 10;
+            end if;
+
+         --If EW red--
+         when 13 =>
+            if EV_Incoming_EW then
+               NextState := 10;
+            elsif EV_Incoming_NS then
+               NextState := 4;
+            elsif (Pedestrian_Wait(North) = 1 and Pedestrian_Wait(South) = 1)
+               and EV_Incoming_NS = False and EV_Incoming_EW = False then
+               NextState := 2;
+            else
+               NextState := 4;
+            end if;
 
          when others =>
             NextState := State + 1;
       end case;
 
-      PreviousState := State;
       State := NextState;
 
    end loop Endless_Loop;
